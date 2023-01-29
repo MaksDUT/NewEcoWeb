f7fd2448ca1f401714431991aa4a9e53749f2151
Max <dumerat.maxime@gmail.com>
2023-01-29 17:41:25 +0100

    add thank page to the route

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import Home from './../routes/Home.svelte'
 import Eco from './../routes/Eco.svelte'
 import NotFound from './../routes/NotFound.svelte'
+import Thanks from './../routes/Thanks.svelte'
 
 const routes = {
     
     '/': Home,
     '/eco': Eco,
+    '/thanks':Thanks,
     '*': NotFound,
 }
 
