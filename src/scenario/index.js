0d02730ee8412251f1410ae3a676a1b21adbb5cd
Max <dumerat.maxime@gmail.com>
2023-02-01 21:16:33 +0100

    Fix css never used

--- src/scenario/index.js
+++ src/scenario/index.js
@@ -4,7 +4,7 @@
 let anwsers = [{response:'La réponse A',isAnwser: false},{response:'La réponse B',isAnwser: false},{response:'La réponse C',isAnwser: false},{response:'La réponse D',isAnwser: true}]
 let presentationVideo = "/videos/test.mp4";
 let anwserVideo = "https://sveltejs.github.io/assets/caminandes-llamigos.mp4";
-let question = "Quel est la réponse à la réponse D ?"
+let question = "Quel est la réponse à la réponse D Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vitae mattis sapien, nec consequat risus. ?"
 let title = "Premier scénario !"
 
 let scenario = {anwsers : anwsers,presentationVideo:presentationVideo,anwserVideo:anwserVideo,question:question,title:title}
